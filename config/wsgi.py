8d99aa5773f1846a780cabf2c75b660d16fa48b0
Binson Woo <bvldog@gmail.com>
2019-11-21 16:06:29 +0900

    add wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,4 +1,4 @@
-"""
+    """
 WSGI config for config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
